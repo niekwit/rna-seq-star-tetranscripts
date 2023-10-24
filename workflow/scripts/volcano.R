945305fb648f82c09590c1da64abcaab3dc4e6fd
niekwit <41302859+niekwit@users.noreply.github.com>
2023-10-24 15:09:04 +0100

    added fc/fdr variables

--- workflow/scripts/volcano.R
+++ workflow/scripts/volcano.R
@@ -1,4 +1,4 @@
-#redirect R output to log
+# redirect R output to log
 log <- file(snakemake@log[[1]], open="wt")
 sink(log, type = "output")
 sink(log, type = "message")
@@ -8,63 +8,70 @@ library(readxl)
 library(ggrepel)
 library(dplyr)
 
+# get xlsx files
+genes.xlsx <- snakemake@input[["genes"]]
+te.xlsx <- snakemake@input[["te"]]
 
-#get output directory
-outdir <- snakemake@output[[1]]
+# get output directories
+outdir_genes <- snakemake@output[["genes"]]
+outdir_te <- snakemake@output[["te"]]
 
-#create output dir
-#dir.create(outdir, showWarnings=FALSE) #snakemake will do this
+# get plotting paramaters
+fdr <- log10(snakemake@params[["fdr"]])
+fc <- snakemake@params[["fc"]]
 
-#for each sheet (i.e. DESeq2 contrast) create volcano plot
-for (i in excel_sheets(snakemake@input[[1]])){
-    #load data for contrast
-    df <- read_excel(snakemake@input[[1]], sheet = i)
+# function to plot volcano for each sheet (i.e. DESeq2 contrast) in given xlsx
+volcano <- function(xlsx, outdir){
+
+  for (i in excel_sheets(xlsx)){
+    # load data for contrast
+    df <- read_excel(xlsx, sheet = i)
   
-    #get contrast name
-    contrast <- unique(df$contrast_name)
+    # get contrast name
+    contrast <- i
     print(paste0("Generating heatmap for ",contrast,"..."))
     
-    #log transform padj
+    # log transform padj
     df$log.padj <- -log10(df$padj)
   
-    #remove genes with NA in log.padj
+    # remove genes with NA in log.padj
     df <- df[!(is.na(df$log.padj)), ]
     
-    #add fill colour based on log2FC and pvalue
+    # add fill colour based on log2FC and pvalue
     df <- df %>%
       mutate(colour = case_when(
-        log2FoldChange > 0.5 & log.padj > 3 ~ "red",
-        log2FoldChange < -0.5 & log.padj > 3 ~ "blue",
-        log2FoldChange < 0.5 & log.padj < 3 ~ "grey40",
-        log2FoldChange > -0.5 & log.padj < 3 ~ "grey40",
+        log2FoldChange > fc & log.padj > fdr ~ "red",
+        log2FoldChange < -fc & log.padj > fdr ~ "blue",
+        log2FoldChange < fc & log.padj < fdr ~ "grey40",
+        log2FoldChange > -fc & log.padj < fdr ~ "grey40",
       ))
     
-    #select top 5 down and up regulated for labels
+    # select top 5 down and up regulated for labels
     df.up <- df %>%
-      filter(log2FoldChange > 0.5) %>%
-      filter(log.padj > 3)
+      filter(log2FoldChange > fc) %>%
+      filter(log.padj > fdr)
     if (nrow(df.up) > 5){
       df.up <- df.up %>% slice(1:5)
     }
       
     df.down <- df %>%
-      filter(log2FoldChange < -0.5) %>%
-      filter(log.padj > 3)
+      filter(log2FoldChange < -fc) %>%
+      filter(log.padj > fdr)
       if (nrow(df.down) > 5){
         df.down <- df.down %>% slice(1:5)
       }
     
     df.label <- rbind(df.up, df.down)
     
-    #create plot
+    # create plot
     p <- ggplot(df, aes(x = `log2FoldChange`,
                    y = `log.padj`)
            ) + 
       theme_bw() +
-      theme(axis.text=element_text(size=16),
-            axis.title=element_text(size=16),
+      theme(axis.text=element_text(size = 16),
+            axis.title=element_text(size = 16),
             plot.title = element_text(hjust = 0.5,
-                                      size=16),
+                                      size = 16),
             axis.line = element_line(colour = "black"),
             panel.border = element_blank(),
             panel.background = element_blank(),
@@ -73,30 +80,35 @@ for (i in excel_sheets(snakemake@input[[1]])){
       geom_point(alpha = 0.5,
                  shape = 21,
                  size = 5,
-                 colour="black",
+                 colour = "black",
                  fill = df$colour) +
       ylab("-log10(adj. p value)") +
-      geom_vline(xintercept = c(-0.5,0.5),
-                 linetype="dashed", 
+      geom_vline(xintercept = c(-fc,fc),
+                 linetype = "dashed", 
                  color = "red", 
-                 linewidth=0.5) +
-      geom_hline(yintercept = 3,
-                 linetype="dashed", 
+                 linewidth = 0.5) +
+      geom_hline(yintercept = fdr,
+                 linetype = "dashed", 
                  color = "red", 
-                 linewidth=0.5) +
+                 linewidth = 0.5) +
       ggtitle(contrast) +
-      geom_label_repel(size=5,
+      geom_label_repel(size = 5,
                        aes(x = `log2FoldChange`,
                            y = `log.padj`,
                            label = external_gene_name), 
                        data = df.label,
-                       nudge_x=-0.125,
-                       nudge_y=0.05) +
+                       nudge_x = -0.125,
+                       nudge_y = 0.05) +
       scale_fill_manual(values = df$`colour`)
       
-      #save plot to file
-      ggsave(paste0(outdir,"/",contrast,".pdf"),p)
+      # save plot to file
+      ggsave(file.path(outdir,paste0(contrast,".pdf")), p)
+  }
 }
+
+# run plotting function for genes and te
+volcano(genes.xlsx, outdir_genes)
+volcano(te.xlsx, outdir_te)
 
 
 sink(log, type = "output")
