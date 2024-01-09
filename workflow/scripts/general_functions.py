721c150a1f3dc7cc670190419666522c5897cbb0
niekwit <41302859+niekwit@users.noreply.github.com>
2024-01-09 11:04:01 +0000

    improved import_samples

--- workflow/scripts/general_functions.py
+++ workflow/scripts/general_functions.py
@@ -1,13 +1,33 @@
 import pandas as pd
-import sys
+import os
 
 def import_samples():
-    try:
-        csv = pd.read_csv("config/samples.csv")
-        SAMPLES = csv["sample"]
+    csv = pd.read_csv("config/samples.csv")
+    SAMPLES = csv["sample"]
+    
+    # check if sample names match file names
+    not_found = []
+    for sample in SAMPLES:
+        r1= f"reads/{sample}_R1_001.fastq.gz"
+        r2= f"reads/{sample}_R2_001.fastq.gz"
+        if not os.path.isfile(r1):
+            not_found.append(r1)
+        if not os.path.isfile(r2):
+            not_found.append(r2)
+    if len(not_found) != 0:
+        not_found = "\n".join(not_found)
+        raise ValueError(f"ERROR: following files not found:\n{not_found}")
         
-        return SAMPLES
+    return SAMPLES    
 
-    except FileNotFoundError:
-        print("ERROR: config/samples.csv not found!")
-        sys.exit(1)
+
+def star_arguments(config):
+    """Returns multimapping arguments for STAR set in config.yaml
+    """
+    ofmn = config["mapping"]["outFilterMultimapNmax"]
+    wamn = config["mapping"]["winAnchorMultimapNmax"]
+    extra_params = config["mapping"]["extra_params"]
+    star_extra = f"--outFilterMultimapNmax {ofmn} --winAnchorMultimapNmax {wamn} {extra_params}"
+    
+    return star_extra
+ 
