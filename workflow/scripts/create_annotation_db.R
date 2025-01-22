201b41a27eff11cdc4ea1931485b1c46b3697476
niekwit <41302859+niekwit@users.noreply.github.com>
2025-01-22 14:37:55 +0000

    added option for T2T

--- workflow/scripts/create_annotation_db.R
+++ workflow/scripts/create_annotation_db.R
@@ -4,14 +4,28 @@ sink(log, type = "output")
 sink(log, type = "message")
 
 library(rtracklayer)
+library(dplyr)
 
 gtf <- snakemake@input[["gtf"]]
+genome <- snakemake@params[["genome"]]
 
 # Gene annotation info
 db <- rtracklayer::import(gtf)
-gene.info <- data.frame(ensembl_gene_id = db$gene_id, 
-                  external_gene_name = db$gene_name, 
-                  gene_biotype = db$gene_biotype)
+
+if (genome == "T2T-CHM13v2.0") {
+  gene.info <- data.frame(gene_id = db$db_xref,
+                          type = db$type,
+                          external_gene_name = db$gene, 
+                          gene_biotype = db$gene_biotype) %>%
+    filter(type == "gene") 
+} else {
+  gene.info <- data.frame(ensembl_gene_id = db$gene_id,
+                          external_gene_name = db$gene_name,
+                          gene_biotype = db$gene_biotype,
+                          gene_id = db$gene_id)
+}
+
+
 
 # Save gene annotation info
 save(gene.info, file = snakemake@output[["edb"]])
