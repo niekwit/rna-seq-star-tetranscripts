22ba027ca752e3eb510a0f6abce3deb15f7276a9
niekwit <41302859+niekwit@users.noreply.github.com>
2025-01-22 14:36:54 +0000

    updated colours section

--- workflow/scripts/pca.R
+++ workflow/scripts/pca.R
@@ -1,4 +1,4 @@
-# redirect R output to log
+# Redirect R output to log
 log <- file(snakemake@log[[1]], open="wt")
 sink(log, type = "output")
 sink(log, type = "message")
@@ -9,35 +9,32 @@ library(RColorBrewer)
 library(ggrepel)
 library(cowplot)
 
-# read in data
 load(snakemake@input[[1]])
 
-# log transform data
+# Log transform data
 rld <- rlog(dds)
 
-# select appropriate colour palette
-if (length(unique(rld$treatment)) == 2) {
+# Select appropriate colour palette
+if (length(unique(rld$treatment)) == 2 or length(unique(rld$treatment)) > 8) {
   palette <- "Paired"
 } else {
   palette <- "Dark2"
 }
 
-#create PCA plot
+# Create PCA plot
 pca <- plotPCA(rld, intgroup=c("genotype", "treatment")) +
   geom_label_repel(aes(label = rld$sample)) + 
   guides(colour = "none") +
   theme_cowplot(16) +
   scale_color_brewer(palette = palette)
 
-# save plot to file
+# Save plot to file
 ggsave(snakemake@output[[1]], 
        pca, 
        width=10,
        height=10)
 
 
-# close redirection of output/messages
+# Close redirection of output/messages
 sink(log, type = "output")
 sink(log, type = "message")
-
-
